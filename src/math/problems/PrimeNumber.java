26fdfff837d734f93eed30fd3758fadc9a852a0e
Halima <dewanbabee@yahoo.ca>
2018-12-23 17:35:33 -0500

    Prime Number done

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -1,5 +1,11 @@
 package math.problems;
 
+import databases.ConnectToSqlDB;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class PrimeNumber {
 
 	public static void main(String[] args) {
@@ -12,6 +18,48 @@ public class PrimeNumber {
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 *
 		 */
+//Create ConnectToSqlDB Object
+		//ConnectToSqlDB consql = new ConnectToSqlDB();
+		try{
+			Connection con = ConnectToSqlDB.connectToSqlDatabase();
+		}
+			catch(SQLException e){
+				System.out.println(e);
+			}
+		catch(ClassNotFoundException e){
+			System.out.println(e);
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+
+		int lowerbound =2;
+		//long higherbound = 1000000L;
+		long higherbound = 5;
+
+
+		prime(lowerbound,higherbound);
+
+	}
+	public static void prime(int l,long h){
+		boolean flag = false;
+
+		for (int i = 1; i <=h ; i++) {
+			for (int j = 2; j < i;j++)
+			{
+				if (i % j == 0) {
+					flag = false;
+					break;
+
+				}
+				else  flag = true;
+
+			}
+			if(flag== true)
+				System.out.println(i);
+
+
+		}
 
 	}
 
