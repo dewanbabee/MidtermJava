885019e363962ce81cb21b40719fa5c648e74a7f
Halima <dewanbabee@yahoo.ca>
2018-12-24 05:41:08 -0500

    commit datastructure problem Datareader

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,5 +1,13 @@
 package datastructure;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 public class DataReader {
 
 	public static void main(String[] args) {
@@ -18,8 +26,42 @@ public class DataReader {
 		 * Use For Each loop/while loop/Iterator to retrieve data.
 		 */
 
-		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car";
+		System.out.println(textFile);
+		Stack<String> stack = new Stack<String>();
+		LinkedList<String> list = new LinkedList<String>();
+		BufferedReader br = null;
+		String fileText = null;
+		try {
+			FileReader fr = new FileReader(textFile);
+			   br = new BufferedReader(fr);
+		}catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
+		try {
+			if (br.ready())
+				fileText = br.readLine();
+		}catch (IOException e) {
+			System.out.println(e);
+		}
+		catch (NullPointerException e)
+		{
+			System.out.println(e);
+		}
+      String [] words = fileText.split(" ");
+		for(int i=0;i<words.length;i++) {
+			System.out.println(words[i]);
+			stack.push(words[i]);
+			list.add(words[i]);
+		}
+		System.out.println("Stack Peek:  "+  stack.peek());
+		System.out.println("Stack Pop:  "+  stack.pop());
+		System.out.println("Stack Search:  "+stack.search("Google"));
 
+		Iterator it = list.iterator();
+		System.out.println("Linked List Display:");
+		while (it.hasNext())
+			System.out.println(it.next());
 
 
 	}
