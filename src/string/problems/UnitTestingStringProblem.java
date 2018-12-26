d97f741c8cc8b6526d65c59ce9f20860d2a191fc
Halima <dewanbabee@yahoo.ca>
2018-12-26 03:25:50 -0500

    done testing of String module

--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -13,75 +13,91 @@ import static org.testng.Assert.assertEquals;
 
 public class UnitTestingStringProblem {
     static PrintStream save_out;
-    static final ByteArrayOutputStream out= new ByteArrayOutputStream(); ;
+    static final ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-  public static void main(String[] args) {
-       save_out = System.out;
-      System.setOut(new PrintStream(out));
+
+    public static void main(String[] args) {
+        save_out = System.out;
+        System.setOut(new PrintStream(out));
         //Apply Unit Test into all the methods in this package.
-        Permutation testPer = new Permutation();
-
-
-        RunTestng rn = new RunTestng();
-
-
-        rn.testPalindrome();
-
-        rn.testNotPalindrome();
 
 
 
+        RunTestng.testPalindrome();
+
+        RunTestng.testNotPalindrome();
+        RunTestng.testPermutation();
+        RunTestng.testDuplicateWord();
+        RunTestng.testDetermineLargestWord();
+        RunTestng.testAnargram();
 
 
-////            testPer.permute("ABC",0,2);
-////            assertEquals("ABC\r\nACB\r\nBAC\r\nBCA\r\nCAB\r\nCBA\r\n", out.toString());
-//            @BeforeTest
-//            String st = Palindrome.palindrome("dad");
-//            assertEquals("This is a Palindrome\r\n",out.toString());
-//
-//            @Test
-//            String st2 = Palindrome.palindrome("add");
-//            assertEquals("This is not a Palindrome\r\n",out.toString());
-
-            System.setOut(save_out);
-            System.out.println("Test Passed");
-
-
+        System.setOut(save_out);
+        System.out.println("Test Passed");
 
 
     }
 
-    public static class RunTestng
-            {
+    public static class RunTestng {
 
 
         @BeforeTest
 
-        public static void testPalindrome()
-        {
+        public static void testPalindrome() {
 
             String st = Palindrome.palindrome("dad");
-            assertEquals( out.toString(), "This is a Palindrome\r\n");
+            assertEquals(out.toString(), "This is a Palindrome\r\n");
             out.reset();
 
             //System.setOut(save_out);
         }
-        @Test
-               public static void testNotPalindrome() {
-        String st2 = Palindrome.palindrome("add");
-      assertEquals( out.toString(),"This is not a Palindrome\r\n");
-            out.reset();
-    }
-    }
-    @AfterTest
-    public static void testPermutation()
-    {
-       Permutation pr = new Permutation();
-        pr.permute("ABC",0,2);
-        assertEquals(out.toString(),"ABC\r\nACB\r\nBAC\r\nBCA\r\nCAB\r\nCBA\r\n");
-        out.reset();
-        System.setOut(save_out);
-        System.out.println("Test Passed");
 
+        @Test
+        public static void testNotPalindrome() {
+            String st2 = Palindrome.palindrome("add");
+            assertEquals(out.toString(), "This is not a Palindrome\r\n");
+            out.reset();
+        }
+
+        @AfterTest
+        public static void testPermutation() {
+            Permutation pr = new Permutation();
+            pr.permute("ABC", 0, 2);
+            assertEquals(out.toString(), "ABC\r\nACB\r\nBAC\r\nBCA\r\nCAB\r\nCBA\r\n");
+            out.reset();
+
+            //System.out.println("Test Permutaion Passed");
+
+        }
+
+        @Test
+        public static void testDuplicateWord() {
+
+            String[] args = {};
+            DuplicateWord.main(args);
+
+
+            assertEquals(out.toString(), "java-->3\r\nis-->3\r\nlanguage-->2\r\n");
+            out.reset();
+
+        }
+
+        @Test
+        public static void testDetermineLargestWord() {
+
+            String st = "Human brain is a biological learning machine";
+            DetermineLargestWord.findTheLargestWord(st);
+            assertEquals(out.toString(), "10 biological\r\n");
+            out.reset();
+            System.setOut(save_out);
+        }
+
+        @Test
+        public static void testAnargram() {
+
+
+            assertEquals(Anagram.isAnagram("ANAGRAM", "MARGANA"), true);
+            System.out.println("Test Anagram passed");
+        }
     }
 }
