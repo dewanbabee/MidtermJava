b5ac69f44a69d8677dab79d6d7615a9d0d038870
Halima <dewanbabee@yahoo.ca>
2018-12-26 03:38:58 -0500

    Sorting Array Testing

--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -9,6 +9,7 @@ public class UnitTestSorting {
      */
 
     public static void main(String[] args) {
+        int[] tempArray = {6,9,2,5,1,0,4};
         int [] unSortedArray = {6,9,2,5,1,0,4};
         int [] sortedArray =   {0,1,2,4,5,6,9};
         //Create Sort object
@@ -23,7 +24,44 @@ public class UnitTestSorting {
         }
 
         //Now implement Unit test for rest of the soring algorithm...................below
+        unSortedArray = tempArray;
+        sort.bubbleSort(unSortedArray);
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Tesing Bubble Sort: Array is not Sorted");
+        }catch(Exception ex){
+            ex.getMessage();
+        }
 
+        unSortedArray = tempArray;
+         sort.mergeSort(unSortedArray);
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
+        unSortedArray = tempArray;
+        sort.bubbleSort(unSortedArray);
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
+        unSortedArray = tempArray;
+        sort.quickSort(unSortedArray);
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+        unSortedArray = tempArray;
+        sort.bubbleSort(unSortedArray);
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+        }catch(Exception ex){
+            ex.getMessage();
+        }
 
     }
 }
